6ed247119c3eaf53ca9e7ea85616635bb820c0af
Alan Anderson <alanpurple@gmail.com>
2018-07-04 17:14:09 +0900

    change dealw2v schema

--- models/DealW2v.js
+++ models/DealW2v.js
@@ -1,20 +1,15 @@
 ﻿const mongoose = require('../set-mongoose');
 const Schema = mongoose.Schema;
 
-const VectorSchema = new Schema({
-    type: Number,
-    values: {
+const DealW2vSchema = new Schema({
+    _id: Number,
+    words: [String],
+    vectorizedWords: {
         type: [Number],
         validate: {
             validator: v => len(v) == 100
         }
     }
-}, { _id: false });
-
-const DealW2vSchema = new Schema({
-    _id: Number,
-    words: [String],
-    vectorizedWords: VectorSchema
 }, { collection: 'dealw2v' });
 
 module.exports = require('../connection').model('DealW2v', DealW2vSchema);
